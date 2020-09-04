d70a7447b2eae69109166525581749111ce26368
sminnaar <sminnaar@student.wethinkcode.co.za>
2020-09-04 10:29:33 +0200

    First commit

---
+++ hypertube/torr/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def stream(request):
+    return HttpResponse('<iframe src="https://archive.org/embed/CC_1916_07_10_TheVagabond" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>')
